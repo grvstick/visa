943428c229fcfa2a23199f2465db05004883c8b3
Spencer Ahn <spencer@elementenergy.com>
2024-04-16 17:39:15 +0900

    change package structure

--- visa_test.go
+++ visa_test.go
@@ -3,12 +3,13 @@
 // Use of this source code is governed by a MIT-style license that
 // can be found in the LICENSE.txt file for the project.
 
-package main
+package visa
 
 import (
 	"errors"
 	"log"
 	"testing"
+
 )
 
 func TestParsingVisaResourceString(t *testing.T) {
